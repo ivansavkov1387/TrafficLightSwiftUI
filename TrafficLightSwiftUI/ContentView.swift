33361900e3f0d9ea7927e6ec73c29a50863ee2e5
Ivan Savkov <ivansavkov1387@gmail.com>
2020-10-16 21:16:09 +0300

    Initial Commit

---
+++ TrafficLightSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TrafficLightSwiftUI
+//
+//  Created by Иван on 10/16/20.
+//  Copyright © 2020 Ivan Savkov. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
